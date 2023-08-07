38e68823511a19c7818e1dc460d60f5ea4534b5b
FabioGomesLima <fabioarturgomesdelima13@gmail.com>
2023-08-07 10:35:49 -0300

    Passagem de parâmetros via métodos GET #9

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from flask import request
 
 @app.route('/')
 @app.route('/index', defaults={"nome":"usuario"})
@@ -12,3 +13,15 @@ def index(nome, profissao, canal):
 @app.route('/contato')
 def contato():
    return render_template('contato.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['GET'])
+def autenticar():
+    usuario = request.args.get ('usuario')
+    senha = request.args.get('senha')
+    return "usuario: {} e senha: {}".format(usuario, senha)
+    
+    
